03b128a2ed05b91450f80312a27514d42277f33e
Jesse S <jelloeater@gmail.com>
2014-02-23 07:02:13 +0500

    finished clock testing?

--- usr/js/realClock.lua
+++ usr/js/realClock.lua
@@ -1,5 +1,26 @@
-
+--Loads API from repo
 os.loadAPI("/bb/api/json")
-str = http.get("http://json-time.appspot.com/time.json").readAll()
-obj = json.decode(str)
-print(obj.firstline)
+
+--Monitor JSONstoreect to output to
+local m = peripheral.wrap("right")
+
+local function run() 
+  while true do
+    local ok, err = pcall(getTime)
+    if not ok then
+      print("Had an issue... " .. err)
+    end
+    sleep(600) -- Sleep for 10 minutes    
+  end
+end
+
+local function getTime()
+	--Clears monitor
+	m.clear()
+	--Gets JSON RawJSONing from web and parse
+	RawJSON = http.get("http://json-time.appspot.com/time.json").readAll()
+	JSONstore = json.decode(RawJSON)
+	m.write(JSONstore.hour+":"+JSONstore.minute)
+end
+
+print("Tick Tock Motherfucker!")
