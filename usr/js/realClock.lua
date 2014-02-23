7d5cf07cd829a8b1478901c54580b2c75d2bbd4c
Jesse S <jelloeater@gmail.com>
2014-02-23 08:15:22 +0500

    updated color and scale, should work

--- usr/js/realClock.lua
+++ usr/js/realClock.lua
@@ -3,8 +3,25 @@ os.loadAPI("/bb/api/json")
 
 --Monitor JSONstoreect to output to
 m = peripheral.wrap("right")
-m.setTextSize(5)
-m.setTextColor("green")
+m.setTextScale(5)
+m.setTextColor(8192)
+
+-- white      1
+-- orange     2   
+-- magenta    4   
+-- lightBlue  8 
+-- yellow     16  
+-- lime       32
+-- pink       64
+-- gray       128   
+-- lightGray  256
+-- cyan       512   
+-- purple     1024
+-- blue       2048  
+-- brown      4096  
+-- green      8192  
+-- red        16384   
+-- black      32768   
 
 
 function run() 
@@ -30,5 +47,4 @@ function getTime()
 	m.write(JSONstore.hour..":"..JSONstore.minute)
 end
 
-run()
-print("Tick Tock Motherfucker!")
+run() --RUN BITCH!
