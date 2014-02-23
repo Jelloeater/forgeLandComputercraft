235e8cbc4eb3389ad78ee48239a86c1b7c0ae037
Jesse S <jelloeater@gmail.com>
2014-02-23 23:54:18 +0500

    Added AM/PM logic

--- usr/js/realClock.lua
+++ usr/js/realClock.lua
@@ -6,23 +6,6 @@ m = peripheral.wrap("right")
 m.setTextScale(4) -- Fits in 2 monitors
 m.setTextColor(8192)
 
--- white      1
--- orange     2   
--- magenta    4   
--- lightBlue  8 
--- yellow     16  
--- lime       32
--- pink       64
--- gray       128   
--- lightGray  256
--- cyan       512   
--- purple     1024
--- blue       2048  
--- brown      4096  
--- green      8192  
--- red        16384   
--- black      32768   
-
 
 function run() 
   while true do
@@ -39,14 +22,46 @@ function getTime()
 	m.clear()
   m.setCursorPos(1,1) --Resets the cursor back to the beginning
 	--Gets JSON RawJSONing from web and parse
+
 	RawJSON = http.get("http://json-time.appspot.com/time.json").readAll()
 	JSONstore = json.decode(RawJSON)
   hourInt = tonumber(JSONstore.hour)
+
+
   hourInt = hourInt + 7 --Sets timezone
+  hourInt = hourInt - 24 --Converts to 12 hr
+
+  amPM ="a" --Default to morning
+
+  if hourInt < 24
+    hourInt - 24
+    amPM = "p"
+  end
+
   hourString = tostring(hourInt)
 
-	m.write(hourString..":"..JSONstore.minute)
-  print(hourString..":"..JSONstore.minute)
+  timeOutput = hourString..":"..JSONstore.minute..amPM
+
+	m.write(timeOutput)
+  print(timeOutput)
 end
 
-run() --RUN BITCH!
+run() --Runs the program
+
+-- Color Refrence
+-- white      1
+-- orange     2   
+-- magenta    4   
+-- lightBlue  8 
+-- yellow     16  
+-- lime       32
+-- pink       64
+-- gray       128   
+-- lightGray  256
+-- cyan       512   
+-- purple     1024
+-- blue       2048  
+-- brown      4096  
+-- green      8192  
+-- red        16384   
+-- black      32768   
