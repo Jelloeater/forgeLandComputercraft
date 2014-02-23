afbf65e684e0ef68b4ffc63a3471302dec0d3cb8
Jesse S <jelloeater@gmail.com>
2014-02-23 07:52:26 +0500

    lets try local vars and objects

--- usr/js/realClock.lua
+++ usr/js/realClock.lua
@@ -2,10 +2,10 @@
 os.loadAPI("/bb/api/json")
 
 --Monitor JSONstoreect to output to
-m = peripheral.wrap("right")
+local m = peripheral.wrap("right")
 
 
-function run() 
+local function run() 
   while true do
     local ok, err = pcall(getTime)
     if not ok then
@@ -15,18 +15,18 @@ function run()
   end
 end
 
-function getTime()
+local function getTime()
 	--Clears monitor
 	m.clear()
 	--Gets JSON RawJSONing from web and parse
 	RawJSON = http.get("http://json-time.appspot.com/time.json").readAll()
 	JSONstore = json.decode(RawJSON)
-  -- hourInt = tonumber(JSONstore.hour)
-  -- hourInt = hourInt + 7 --Sets timezone
-  -- hourString = tostring(hourInt)
+  hourInt = tonumber(JSONstore.hour)
+  hourInt = hourInt + 7 --Sets timezone
+  hourString = tostring(hourInt)
 
 	m.write(JSONstore.hour..":"..JSONstore.minute)
 end
 
-print("Tick Tock Motherfucker!")
 run()
+print("Tick Tock Motherfucker!")
